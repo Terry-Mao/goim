19880b782ce6a0f7552cff986e99ece933663881
felix hao <felix@felixdeMacBook-Pro.local>
2015-05-20 17:33:03 +0800

    modify test

--- router/client/main.go
+++ router/client/main.go
@@ -6,14 +6,15 @@ import (
 	"fmt"
 	"math"
 	"net/rpc"
+	"strings"
 	"time"
 )
 
 const (
-	TEST_NUM = 1
+	TEST_NUM = 100
 
-	SUB_DATA_NUM   = 2048000
-	TOPIC_DATA_NUM = 512
+	SUB_DATA_NUM   = 204800000
+	TOPIC_DATA_NUM = 1024
 
 	SUB_KEY   = "sub_id_Terry-Mao_%d"
 	TOPIC_KEY = "topic_id_Felix-Hao_%d"
@@ -22,7 +23,9 @@ const (
 )
 
 var (
-	addr string
+	addrs string
+
+	addrArr []string
 
 	stopCh = make(chan struct{}, 3)
 )
@@ -45,11 +48,13 @@ type RPCSubMsg struct {
 }
 
 func init() {
-	flag.StringVar(&addr, "a", "127.0.0.1:9090", "rpc address")
+	flag.StringVar(&addrs, "a", "10.57.65.26:9090,10.57.65.26:9091,10.57.65.26:9092,10.57.65.26:9093", "rpc address")
 }
 
 func main() {
 	flag.Parse()
+	addrArr = strings.Split(addrs, ",")
+
 	initData()
 	testSub()
 	testBatchSub()
@@ -72,75 +77,90 @@ func stop(bus string, counts []int64) {
 }
 
 func testSub() {
-	c, err := rpc.Dial("tcp", addr)
-	if err != nil {
-		log.Error("rpc.Dial error(%v)", err)
-		return
-	}
-	counts := make([]int64, TEST_NUM)
-	for i := 0; i < TEST_NUM; i++ {
-		go func(count *int64) {
-			ret := &RPCSubMsg{}
-			for i := 0; i < math.MaxInt64; i++ {
-				key := fmt.Sprintf(SUB_KEY, i)
-				c.Call("RouterRPC.Sub", &key, ret)
-				*count++
-			}
-		}(&counts[i])
+	counts := make([]int64, len(addrArr)*TEST_NUM)
+	ci := 0
+	for _, v := range addrArr {
+		for i := 0; i < TEST_NUM; i++ {
+			go func(count *int64) {
+				c, err := rpc.Dial("tcp", v)
+				if err != nil {
+					log.Error("rpc.Dial error(%v)", err)
+					return
+				}
+				ret := &RPCSubMsg{}
+				for i := 0; i < math.MaxInt64; i++ {
+					key := fmt.Sprintf(SUB_KEY, i)
+					c.Call("RouterRPC.Sub", &key, ret)
+					*count++
+				}
+			}(&counts[ci])
+			ci++
+		}
 	}
 	stop("sub", counts)
 }
 
 func testBatchSub() {
-	c, err := rpc.Dial("tcp", addr)
-	if err != nil {
-		log.Error("rpc.Dial error(%v)", err)
-		return
-	}
-	counts := make([]int64, TEST_NUM)
-	for i := 0; i < TEST_NUM; i++ {
-		go func(count *int64) {
-			const (
-				keys_num = 50
-			)
-			ret := &RPCSubMsg{}
-			keys := make([]string, keys_num)
-			for i := 0; i < math.MaxInt64; i++ {
-				k := 0
-				for j := i; j < i+keys_num; j++ {
-					keys[k] = fmt.Sprintf(SUB_KEY, j)
-					k++
+	counts := make([]int64, len(addrArr)*TEST_NUM)
+	ci := 0
+	for _, v := range addrArr {
+		for i := 0; i < TEST_NUM; i++ {
+			go func(count *int64) {
+				const (
+					keys_num = 50
+				)
+				c, err := rpc.Dial("tcp", v)
+				if err != nil {
+					log.Error("rpc.Dial error(%v)", err)
+					return
 				}
-				c.Call("RouterRPC.BatchSub", &keys, ret)
-				*count++
-			}
-		}(&counts[i])
+				ret := &RPCSubMsg{}
+				keys := make([]string, keys_num)
+				for i := 0; i < math.MaxInt64; i++ {
+					k := 0
+					for j := i; j < i+keys_num; j++ {
+						keys[k] = fmt.Sprintf(SUB_KEY, j)
+						k++
+					}
+					c.Call("RouterRPC.BatchSub", &keys, ret)
+					*count++
+				}
+			}(&counts[ci])
+			ci++
+		}
 	}
 	stop("batch sub", counts)
 }
 
 func testTopic() {
-	c, err := rpc.Dial("tcp", addr)
-	if err != nil {
-		log.Error("rpc.Dial error(%v)", err)
-		return
-	}
-	counts := make([]int64, TEST_NUM)
-	for i := 0; i < TEST_NUM; i++ {
-		go func(count *int64) {
-			ret := &RPCSubMsg{}
-			for i := 0; i < math.MaxInt64; i++ {
-				key := fmt.Sprintf(TOPIC_KEY, i)
-				c.Call("RouterRPC.Topic", &key, ret)
-				*count++
-			}
-		}(&counts[i])
+	counts := make([]int64, len(addrArr)*TEST_NUM)
+	ci := 0
+	for _, v := range addrArr {
+		for i := 0; i < TEST_NUM; i++ {
+			go func(count *int64) {
+				c, err := rpc.Dial("tcp", v)
+				if err != nil {
+					log.Error("rpc.Dial error(%v)", err)
+					return
+				}
+				ret := &RPCSubMsg{}
+				for i := 0; i < math.MaxInt64; i++ {
+					key := fmt.Sprintf(TOPIC_KEY, i)
+					c.Call("RouterRPC.Topic", &key, ret)
+					*count++
+				}
+			}(&counts[ci])
+			ci++
+		}
 	}
 	stop("topic", counts)
 }
 
 func initData() {
-	c, err := rpc.Dial("tcp", addr)
+	if len(addrArr) == 0 {
+		panic("addrs is empty")
+	}
+	c, err := rpc.Dial("tcp", addrArr[0])
 	if err != nil {
 		log.Error("rpc.Dial error(%v)", err)
 		return
