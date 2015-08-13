b405484e6d87dc3bbc7b100256b2c4f527acb171
liuding <liuding@bilibili.com>
2015-08-13 19:40:56 +0800

    fix job rpc connect

--- logic/job/pushs/router.go
+++ logic/job/pushs/router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	log "code.google.com/p/log4go"
+	inet "github.com/Terry-Mao/goim/libs/net"
 	proto "github.com/Terry-Mao/goim/proto/router"
 	"github.com/Terry-Mao/gopush-cluster/ketama"
 	"github.com/Terry-Mao/protorpc"
@@ -20,41 +21,55 @@ const (
 )
 
 var (
-	routerServiceMap = map[string]*protorpc.Client{}
+	routerServiceMap = map[string]**protorpc.Client{}
 	routerRing       *ketama.HashRing
 	routerQuit       = make(chan struct{}, 1)
 )
 
 func InitRouter() (err error) {
 	var (
-		r *rpc.Client
-		i = 0
+		network, addr string
 	)
 	routerRing = ketama.NewRing(ketama.Base)
-	for serverId, addr := range Conf.RouterRPCAddrs {
-		r, err = rpc.Dial(Conf.RouterRPCNetworks[i], addr)
-		if err != nil {
-			log.Error("rpc.Dial(\"%s\", \"%s\") error(%s)", Conf.RouterRPCNetworks[i], addr, err)
+	for serverId, addrs := range Conf.RouterRPCAddrs {
+		// WARN r must every recycle changed for reconnect
+		var (
+			r          *rpc.Client
+			routerQuit = make(chan struct{}, 1)
+		)
+		if network, addr, err = inet.ParseNetwork(addrs); err != nil {
+			log.Error("inet.ParseNetwork() error(%v)", err)
 			return
 		}
-		go rpc.Reconnect(&r, routerQuit, Conf.RouterRPCNetworks[i], addr)
+		r, err = rpc.Dial(network, addr)
+		if err != nil {
+			log.Error("rpc.Dial(\"%s\", \"%s\") error(%s)", network, addr, err)
+		}
+		go rpc.Reconnect(&r, routerQuit, network, addr)
 		log.Debug("router rpc addr:%s connect", addr)
-		routerServiceMap[serverId] = r
+		routerServiceMap[serverId] = &r
 		routerRing.AddNode(serverId, 1)
-		i++
 	}
 	routerRing.Bake()
 	return
 }
 
-func getRouterClient(userID int64) *protorpc.Client {
+func getRouterClient(userID int64) (*protorpc.Client, error) {
 	node := routerRing.Hash(strconv.FormatInt(userID, 10))
-	return routerServiceMap[node]
+	if client, ok := routerServiceMap[node]; !ok || *client == nil {
+		return nil, ErrRouter
+	} else {
+		return *client, nil
+	}
 }
 
 // 获取在线数量
 func getOnlineCount(userID int64) (count int32, err error) {
-	c := getRouterClient(userID)
+	c, err := getRouterClient(userID)
+	if err != nil {
+		log.Error("getRouterClient(\"%d\") error(%v)", userID, err)
+		return
+	}
 	arg := &proto.GetSeqCountArg{UserId: userID}
 	reply := &proto.GetSeqCountReply{}
 	if err = c.Call(routerServiceGetSeqCount, arg, reply); err != nil {
