890b3908daee7d7107375ac745f9afaac4fc7e78
Terry.Mao <83873308@qq.com>
2015-08-28 15:59:13 +0800

    remove no used config key

--- logic/job/config.go
+++ logic/job/config.go
@@ -17,26 +17,22 @@ func init() {
 }
 
 type Config struct {
-	Log               string            `goconf:"base:log"`
-	RouterAddrs       []string          `goconf:"router:addr:,"`
-	ZKAddrs           []string          `goconf:"kafka:zookeeper.list:,"`
-	ZKRoot            string            `goconf:"kafka:zkroot"`
-	KafkaTopic        string            `goconf:"kafka:topic"`
-	Comets            map[int32]string  `goconf:"-"`
-	RouterRPCNetworks []string          `goconf:"router:networks:,"`
-	PushChan          int               `goconf:"push:chan"`
-	PushChanSize      int               `goconf:"push:chan.size"`
-	RouterRPCAddrs    map[string]string `-`
+	Log          string           `goconf:"base:log"`
+	ZKAddrs      []string         `goconf:"kafka:zookeeper.list:,"`
+	ZKRoot       string           `goconf:"kafka:zkroot"`
+	KafkaTopic   string           `goconf:"kafka:topic"`
+	Comets       map[int32]string `goconf:"-"`
+	PushChan     int              `goconf:"push:chan"`
+	PushChanSize int              `goconf:"push:chan.size"`
 }
 
 func NewConfig() *Config {
 	return &Config{
-		Comets:         make(map[int32]string),
-		ZKRoot:         "",
-		KafkaTopic:     "kafka_topic_push",
-		RouterRPCAddrs: make(map[string]string),
-		PushChan:       4,
-		PushChanSize:   100,
+		Comets:       make(map[int32]string),
+		ZKRoot:       "",
+		KafkaTopic:   "kafka_topic_push",
+		PushChan:     4,
+		PushChanSize: 100,
 	}
 }
 
