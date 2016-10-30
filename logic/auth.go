4ec36a15dec322526ac0a31b142c23a5e5ca73f3
wozitaoran <xurui678678@163.com>
2016-10-30 11:10:40 +0800

    auth.go

--- logic/auth.go
+++ logic/auth.go
@@ -18,12 +18,23 @@ func NewDefaultAuther() *DefaultAuther {
 }
 
 func (a *DefaultAuther) Auth(token string) (userId int64, roomId int32) {
+
 	var err error
-	if userId, err = strconv.ParseInt(token, 10, 64); err != nil {
+
+	var token0, token1 string
+	token0 = strings.Split(token, ",")[0]
+
+	token1 = strings.Split(token, ",")[1]
+
+	if userId, err = strconv.ParseInt(token0, 10, 64); err != nil {
 		userId = 0
+	}
+	var roomIdTemp int64
+	if roomIdTemp, err = strconv.ParseInt(token1, 10, 64); err != nil {
 		roomId = define.NoRoom
 	} else {
-		roomId = 1 // only for debug
+		roomId = int32(roomIdTemp)
 	}
+
 	return
 }
