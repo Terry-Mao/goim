180fa9423895e89299e284f92d610de41fafac97
Zhou Weitong <13832753+zhouweitong3@users.noreply.github.com>
2018-05-09 19:10:57 +0800

    Add CORS.

--- logic/http.go
+++ logic/http.go
@@ -76,6 +76,9 @@ func retPWrite(w http.ResponseWriter, r *http.Request, res map[string]interface{
 }
 
 func Push(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
@@ -131,6 +134,9 @@ func parsePushsBody(body []byte) (msg []byte, userIds []int64, err error) {
 
 // {"m":{"test":1},"u":"1,2,3"}
 func Pushs(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
@@ -169,6 +175,9 @@ func Pushs(w http.ResponseWriter, r *http.Request) {
 }
 
 func PushRoom(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
@@ -206,6 +215,9 @@ func PushRoom(w http.ResponseWriter, r *http.Request) {
 }
 
 func PushAll(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
@@ -244,6 +256,9 @@ type ServerCounter struct {
 }
 
 func Count(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "GET" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
@@ -270,6 +285,9 @@ func Count(w http.ResponseWriter, r *http.Request) {
 }
 
 func DelServer(w http.ResponseWriter, r *http.Request) {
+	if CORSOpen {
+		w.Header().Set("Access-Control-Allow-Origin", CORSAddr) 
+	}
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", 405)
 		return
