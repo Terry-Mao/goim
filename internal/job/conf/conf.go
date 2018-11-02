7f7a3545828a112e27bcc5721e73005d203ab67d
chenzhihui <i@tony.wiki>
2018-11-02 20:54:40 +0800

    add job struct

--- internal/job/conf/conf.go
+++ internal/job/conf/conf.go
@@ -1,1 +1,60 @@
 package conf
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	confPath string
+	// Conf config
+	Conf = &Config{}
+)
+
+// Config is job config.
+type Config struct {
+	Env *Env
+}
+
+// Env is env config.
+type Env struct {
+	Region string
+	Zone   string
+	Env    string
+	Host   string
+}
+
+func (e *Env) fix() {
+	if e.Region == "" {
+		e.Region = os.Getenv("REGION")
+	}
+	if e.Zone == "" {
+		e.Zone = os.Getenv("ZONE")
+	}
+	if e.Env == "" {
+		e.Env = os.Getenv("DEPLOY_ENV")
+	}
+	if e.Host == "" {
+		e.Host, _ = os.Hostname()
+	}
+}
+
+// Init init conf
+func Init() error {
+	return local()
+}
+
+func local() (err error) {
+	if _, err = toml.DecodeFile(confPath, &Conf); err != nil {
+		return
+	}
+	Conf.fix()
+	return
+}
+func (c *Config) fix() {
+	if c.Env == nil {
+		c.Env = new(Env)
+	}
+	c.Env.fix()
+}
--- internal/job/job.go
+++ internal/job/job.go
@@ -1,1 +1,16 @@
 package job
+
+import "github.com/Terry-Mao/goim/internal/job/conf"
+
+// Job is push job.
+type Job struct {
+	c *conf.Config
+}
+
+// New new a push job.
+func New(c *conf.Config) *Job {
+	j := &Job{
+		c: c,
+	}
+	return j
+}
