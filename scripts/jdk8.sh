08e36d2feaa7f5c8386d977887af8560f2c03a8b
chenzhihui <i@tony.wiki>
2018-12-17 19:54:55 +0800

    update script

--- scripts/jdk8.sh
+++ scripts/jdk8.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# config parameters
+JDK_HOME=/usr/local/jdk8/
+# download jdk
 curl -L https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz -o jdk-8u191-linux-x64.tar.gz
-t
 tar zxf jdk-8u191-linux-x64.tar.gz 
-mkdir -p /usr/local/jdk8/
-mv jdk1.8.0/* /usr/local/jdk8/
-update-alternatives --install "/usr/bin/java" "java" "/usr/local/jdk8/bin/java" 1500
-update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/jdk8/bin/javac" 1500
-update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/local/jdk8/bin/javaws" 1500
+# install jdk
+mkdir -p $JDK_HOME
+mv jdk1.8.0/* $JDK_HOME
+update-alternatives --install "/usr/bin/java" "java" "$JDK_HOME/bin/java" 1500
+update-alternatives --install "/usr/bin/javac" "javac" "$JDK_HOME/bin/javac" 1500
+update-alternatives --install "/usr/bin/javaws" "javaws" "$JDK_HOME/bin/javaws" 1500
--- scripts/kafka.sh
+++ scripts/kafka.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
-usage='./kafka.sh brokeid\r\neg: ./kafka.sh 1'
-if [ ! "$1" ];then 
-echo "kafka broke.id can not be null"
-echo -e $usage
+# config parameters
+JDK_HOME=/usr/local/jdk8/
+KAFKA_HOME=/data/app/kafka/
+KAFKA_CONF=/data/app/kafka/config/server.properties 
+KAFKA_DATA=/data/kafka_data/
+KAFKA_SUPERVISOR=/etc/supervisor/conf.d/kafka.conf
+
+# check parammeters
+BROKER_ID=$1
+CURRENT_ADDR=$2
+ZK_ADDRS=$3
+if [ ! "$BROKER_ID" ]; then
+echo "kafka {broker.id} can not be null"
+echo -e "./kafka.sh {broker.id} {current.addr} {zk.addr1,zk.addr2,zk.addr3}\r\neg: ./kafka.sh 1 10.1.0.10 10.1.0.10:2181,10.1.0.11:2181,10.1.0.12:2181"
 exit 1
 fi
-jdk8="/usr/local/jdk8"
-if [ ! -d "$jdk8" ];then
+if [ ! "$CURRENT_ADDR" ]; then
+echo "zookeeper path can not be null"
+exit 1
+fi
+if [ ! "$ZK_ADDRS" ]; then
+echo "zookeeper path can not be null"
+exit 1
+fi
+if [ ! -d "$JDK_HOME" ]; then
 echo "jdk not exist down jdk8"
 ./jdk8.sh
 fi
-mkdir repo
-cd repo 
+
+# download kafka
 curl "http://apache.stu.edu.tw/kafka/2.1.0/kafka_2.11-2.1.0.tgz"  -o kafka_2.11-2.1.0.tgz
 tar zxf kafka_2.11-2.1.0.tgz
-mkdir -p /data/kafka_2.11-2.1.0
-mv kafka_2.11-2.1.0/* /data/kafka_2.11-2.1.0
-
-#set kafka conf 
-mkdir -p /data/kafka_2.11-2.1.0/config/databus-kafka
-kafka_conf=/data/kafka_2.11-2.1.0/config/databus-kafka/server.properties 
-zk_path=""  #write you zk cluster  path here.
-if [ ! "$zk_path" ];then
-echo "zkpath can not be null"
-exit 1
-fi
-echo "broker.id=$1">$kafka_conf
-echo "host.name=$host">>$kafka_conf
-echo "zookeeper.connect=$zk_path/kafka">>$kafka_conf
-echo 'num.network.threads=4
+# install kafka
+mkdir -p $KAFKA_HOME
+mkdir -p $KAFKA_DATA
+mv kafka_2.11-2.1.0/* $KAFKA_HOME
+# kafka config
+echo "broker.id=$BROKER_ID">$KAFKA_CONF
+echo "host.name=$CURRENT_ADDR">>$KAFKA_CONF
+echo "zookeeper.connect=$ZK_ADDRS/kafka">>$KAFKA_CONF
+echo "num.network.threads=4
 num.io.threads=4
 socket.send.buffer.bytes=1024000
 socket.receive.buffer.bytes=1024000
 socket.request.max.bytes=52428800
-log.dirs=/data/lib/kafka/kafka_platform
-num.partitions=2
+log.dirs=$KAFKA_DATA
+num.partitions=9
 num.recovery.threads.per.data.dir=1
 log.cleanup.policy=delete
 log.retention.hours=24
@@ -43,14 +54,18 @@ log.retention.check.interval.ms=300000
 log.cleaner.enable=false
 zookeeper.connection.timeout.ms=6000
 default.replication.factor=2
-#delete.topic.enable=true
-auto.create.topics.enable=true'>>$kafka_conf
-### start by supervisor
-apt-get install -y supervisor
-supervisor_path=/etc/supervisor/conf.d/kafka.conf
+delete.topic.enable=false
+auto.create.topics.enable=false">>$KAFKA_CONF
 
-echo '[program:kafka]
-command=/data/kafka_2.11-2.1.0/bin/kafka-server-start.sh /data/kafka_2.11-2.1.0/config/databus-kafka/server.properties
+# install supervisor
+if ! type "supervisorctl" > /dev/null; then
+    apt-get install -y supervisor
+fi
+# kafka supervisor config
+mkdir -p /etc/supervisor/conf.d/
+mkdir -p /data/log/kafka/
+echo "[program:kafka]
+command=$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_CONF
 user=root
 autostart=false
 autorestart=true
@@ -58,13 +73,12 @@ startretries=10
 exitcodes=0
 stopsignal=KILL
 stopwaitsecs=10
-stdout_logfile=/data/log/kafka/databus/stdout.log
-stderr_logfile=/data/log/kafka/databus/stderr.log
+stdout_logfile=/data/log/kafka/stdout.log
+stderr_logfile=/data/log/kafka/stderr.log
 stdout_logfile_maxbytes=100MB
 stdout_logfile_backups=5
 stderr_logfile_maxbytes=100MB
 stderr_logfile_backups=5
-environment=JAVA_HOME="/usr/local/jdk8",JRE_HOME="/usr/local/jdk8/jre",KAFKA_HEAP_OPTS="-Xmx6g -Xms6g -XX:MetaspaceSize=96m -XX:G1HeapRegionSize=16M -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80"'>$supervisor_path
+environment=JAVA_HOME=$JDK_HOME,JRE_HOME='$JDK_HOME/jre',KAFKA_HEAP_OPTS='-Xmx6g -Xms6g -XX:MetaspaceSize=96m -XX:G1HeapRegionSize=16M -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80'">$_KAFKA_SUPERVISOR
 
-mkdir -p /data/log/kafka/databus
 supervisorctl update
