86be0c02f208fd89a5be1971ddb8ab969c3788ce
JinLin Fu <248628578@qq.com>
2019-04-16 09:53:20 +0800

    Update dao.go

--- internal/logic/dao/dao.go
+++ internal/logic/dao/dao.go
@@ -29,7 +29,6 @@ func New(c *conf.Config) *Dao {
 }
 
 func newKafkaPub(c *conf.Kafka) kafka.SyncProducer {
-	var err error
 	kc := kafka.NewConfig()
 	kc.Producer.RequiredAcks = kafka.WaitForAll // Wait for all in-sync replicas to ack the message
 	kc.Producer.Retry.Max = 10                  // Retry up to 10 times to produce the message
